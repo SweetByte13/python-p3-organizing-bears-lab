0266dbc96dc78a0581a06683c529cccbdc49c9c5
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-05-20 10:01:17 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "brown", "sweet", 0),
+("Rowdy", 2, "M", "black", "mean", 0),
+("Tabitha", 2, "F", "brown", "sweet", 1),
+("Sergeant Brown", 1, "M", "brown", "sweet", 0),
+("Melissa", 4, "M", "black", "mean", 1),
+("Grinch", 3, "F", "brown", "sweet", 0),
+("Wendy", 4, "M", "black", "mean", 1),
+(NULL, 5, "F", "brown", "sweet", 0)
